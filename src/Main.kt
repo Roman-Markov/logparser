704c520cdb8637b8fd7871c097835ab1e2889f40
Roman Markov <r_a_markov@mail.ru>
2018-12-19 13:33:56 +0300

    add project settings under version control

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,38 @@
-class Main {
+import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
+import java.util.*
+import java.util.regex.Pattern
 
-    fun main(args :Array<String>) {
-        println("hi all")
+fun main(args :Array<String>) {
+
+    val inFile = File("log.txt")
+    inFile.createNewFile();
+    val outFile = File("outlog.txt")
+    outFile.createNewFile()
+
+    val originLogStream = FileReader("log.txt")
+    val regexpForCut = "OkHttp http"
+    val remainingStringList: List<String> = getNeededStringList(originLogStream, regexpForCut)
+    val outputFileName = "outlog.txt"
+    writeOutLog(outputFileName, remainingStringList);
+}
+
+fun getNeededStringList(fileReader: FileReader, regexp: String): List<String> {
+    var lines: List<String> = ArrayList<String>()
+    fileReader.use { it ->
+        val pattern: Pattern = Pattern.compile(regexp)
+        lines = it.readLines().filter { item ->  !pattern.matcher(item).find() }
+    }
+    return lines;
+}
+
+fun writeOutLog(outFileName: String, stringList: List<String>): Unit {
+    val fileWriter = FileWriter(outFileName)
+    fileWriter.use {
+        for (item in stringList) {
+            it.write(item)
+            it.write("\n")
+        }
     }
 }
